75e463fb0bae3745ff8a6e11219e3493d4af973b
neozoid <lindsey.s.seay@gmail.com>
2023-10-14 14:08:37 -0400

    functional image generation

--- next.config.js
+++ next.config.js
@@ -1,4 +1,18 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {}
+const nextConfig = {
+  reactStrictMode: true,
+  images: {
+    remotePatterns: [
+      {
+        protocol: "https",
+        hostname: "replicate.com",
+      },
+      {
+        protocol: "https",
+        hostname: "pbxt.replicate.delivery",
+      },
+    ],
+  },
+};
 
 module.exports = nextConfig
