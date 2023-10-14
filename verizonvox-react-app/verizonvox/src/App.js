72391223a9ae1e4a870638681fb47cde92e23b01
Grace Hu <gracehu2003@gmail.com>
2023-10-14 17:11:43 -0400

    added grey and white sections

--- verizonvox-react-app/verizonvox/src/App.js
+++ verizonvox-react-app/verizonvox/src/App.js
@@ -9,7 +9,12 @@ const App = () => {
     <div className="app">
       <Header />
       {/* Your main app content goes here */}
-      <h1>Tablets and Computers</h1>
+      <div className = "white-section">
+        <h1>Tablets and Computers</h1>
+      </div>
+      <div className = "grey-section">
+        <div className="divider"></div>
+      </div>
     </div>
   );
 };
