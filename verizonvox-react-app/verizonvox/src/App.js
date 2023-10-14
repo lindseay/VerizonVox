77ea4269e8e70482ca1b993de2acca0fabbacec3
Grace Hu <gracehu2003@gmail.com>
2023-10-14 15:57:31 -0400

    simple black header

---
+++ verizonvox-react-app/verizonvox/src/App.js
@@ -0,0 +1,15 @@
+import React from 'react';
+import Header from './components/Header.js';
+import 'bootstrap/dist/css/bootstrap.min.css';
+
+
+const App = () => {
+  return (
+    <div className="app">
+      <Header />
+      {/* Your main app content goes here */}
+    </div>
+  );
+};
+
+export default App;
---
+++ verizonvox-react-app/verizonvox/src/components/Header.js
@@ -0,0 +1,14 @@
+import React from 'react';
+
+const Header = () => {
+  return (
+    <nav className="navbar navbar-dark bg-dark py-4">
+      <div className="container">
+        <span className="navbar-brand">hi</span>
+        {/* Add navigation links or other content here */}
+      </div>
+    </nav>
+  );
+};
+
+export default Header;
