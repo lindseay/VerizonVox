a78db4ca6b39c6543db0bf0cb8183ccdbb129e23
Grace Hu <gracehu2003@gmail.com>
2023-10-15 10:03:16 -0400

    added image_directory element to pictureComponents array

--- verizonvox-react-app/verizonvox/src/App.js
+++ verizonvox-react-app/verizonvox/src/App.js
@@ -2,24 +2,30 @@ import Header from './components/Header.js';
 import 'bootstrap/dist/css/bootstrap.min.css';
 import './App.css';
 import React, { useState, useEffect, useRef} from 'react';
+import { BrowserRouter as Router, Route, Link, Switch } from 'react-router-dom';
+// Product Listing components 
 import ProductListing1 from './components/ProductListing1.js';
 import ProductListing2 from './components/ProductListing2.js';
 import ProductListing3 from './components/ProductListing3.js';
 import ProductListing4 from './components/ProductListing4.js';
 import ProductListing5 from './components/ProductListing5.js';
 import ProductListing6 from './components/ProductListing6.js';
+// Product Listing component pages 
+import ProductListing1Page from './components/ProductListing1Page.js';
+import ProductListing2Page from './components/ProductListing2Page.js';
+
 
 const App = () => {
   const [selectedIdx, setSelectedIdx] = useState(0);
   const selectedIdxRef = useRef(selectedIdx);
 
   const pictureComponents = [
-    <ProductListing1 key={0} isSelected={selectedIdx === 0} />,
-    <ProductListing2 key={1} isSelected={selectedIdx === 1} />,
-    <ProductListing3 key={2} isSelected={selectedIdx === 2} />,
-    <ProductListing4 key={3} isSelected={selectedIdx === 3} />,
-    <ProductListing5 key={4} isSelected={selectedIdx === 4} />,
-    <ProductListing6 key={5} isSelected={selectedIdx === 5} />,
+    <ProductListing1 key={0} isSelected={selectedIdx === 0} image_directory={"../public/verizon-product-images/apple-ipad-10th-generation.png"}/>,
+    <ProductListing2 key={1} isSelected={selectedIdx === 1} image_directory={"../public/verizon-product-images/orbic-airsurf.png"}/>,
+    <ProductListing3 key={2} isSelected={selectedIdx === 2} image_directory={"../public/verizon-product-images/orbic-chromebook.png"}/>,
+    <ProductListing4 key={3} isSelected={selectedIdx === 3} image_directory={"../public/verizon-product-images/razer-edge-5g.png"}/>,
+    <ProductListing5 key={4} isSelected={selectedIdx === 4} image_directory={"../public/verizon-product-images/samsung-galaxy-tab-a7lite.png"}/>,
+    <ProductListing6 key={5} isSelected={selectedIdx === 5} image_directory={"../public/verizon-product-images/samsung-galaxy-tab-s85g.png"}/>,
   ];
 
   useEffect(() => {
