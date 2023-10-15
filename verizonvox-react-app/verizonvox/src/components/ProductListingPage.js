2eb9958e4ac932cb5b0aff4bd92fe7b9a3f223da
Olivia <oliviabronstein@ufl.edu>
2023-10-15 13:47:16 -0400

    update product listing page

---
+++ verizonvox-react-app/verizonvox/src/components/ProductListingPage.js
@@ -0,0 +1,24 @@
+import React from 'react';
+import './styles.css';
+import Header from './Header.js';
+
+const ProductListing1Page = (props) => {
+
+  const {title, description, image } = props;
+
+  const productRightSideDetailsImageUrl = process.env.PUBLIC_URL + '/verizon-product-images/product-pages-right-side.png';
+  return (
+    <div>
+        <Header/>
+        <h1 style={{ fontWeight: 'bold' }}> { title } </h1>
+        <div className = "horizontal">
+        <img src={image} alt="image 1" className="product-page-image"/>
+        <img src={productRightSideDetailsImageUrl} className = "product-page-right-side-image"/>
+        </div>
+        <div className="horizontal-divider"></div>
+        <h6 className="product-page-description">{ description }</h6>
+    </div>
+  );
+};
+
+export default ProductListing1Page;
