1b73a7af0b4782417306dbe1a05ecec7cd17e01b
Grace Hu <gracehu2003@gmail.com>
2023-10-15 12:35:40 -0400

    finished product page design

--- verizonvox-react-app/verizonvox/src/components/ProductListing1.js
+++ verizonvox-react-app/verizonvox/src/components/ProductListing1.js
@@ -3,12 +3,12 @@ import './styles.css';
 import { Link } from 'react-router-dom';
 
 const ProductListing1 = () => {
-  const imageUrl = process.env.PUBLIC_URL + '/verizon-product-images/apple-ipad-10th-generation.png';
+  const productImageUrl = process.env.PUBLIC_URL + '/verizon-product-images/apple-ipad-10th-generation.png';
   return (
     <Link to="/productlisting1page" className="link-no-underline">
     <div className="rounded-image-container">
         
-        <img src={imageUrl} alt="image 1" className= "shrink-image" />
+        <img src={productImageUrl} alt="image 1" className= "shrink-image" />
         <div className="listing-info">
             <h5 style={{ fontWeight: 'bold' }}> Apple iPad (10th Generation) </h5>
             <h6 style={{ color: 'grey' }}>Retail price: $599.99</h6>
