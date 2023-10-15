62eb4e4112e826494abf106ade296eec1142fb8a
neozoid <lindsey.s.seay@gmail.com>
2023-10-15 15:08:11 -0400

    broken code

--- verizonvox-react-app/verizonvox/src/components/ProductListingPage.js
+++ verizonvox-react-app/verizonvox/src/components/ProductListingPage.js
@@ -10,48 +10,48 @@ const ProductListing1Page = (props) => {
   const msg = new SpeechSynthesisUtterance();
   const [prediction, setPrediction] = useState(null);
   const [error, setError] = useState(null);
+  const [data, setData] = useState(null);
   const replicate = new Replicate({
     auth: process.env.REPLICATE_API_TOKEN,
   });
 
   const handleSubmit = async (e) => {
-    
-    /*
-    let image_source = e.nativeEvent.target.src;
-    let query = image_source.split("?url=")[1];
-    query = query.split("&")[0];
-    query = query.replaceAll("%2F", "/");
-    query = query.replaceAll("%3A", ":");
-    */
     let query = "https://i.postimg.cc/gkM73gkt/laptop.png";
     e.preventDefault();
-    const response = await fetch("https://api.replicate.com/v1/api/predictions", {
-      mode: "no-cors",
-      method: "POST",
-      headers: {
-        "Content-Type": "application/json",
-        Authorization: `Token ${process.env.REPLICATE_API_TOKEN}`,
-      },
-      body: JSON.stringify({
-        //image: "https://avatars.githubusercontent.com/u/147882946?v=4"
-        //image: "https://replicate.delivery/mgxm/8b4d747d-feca-477d-8069-ee4d5f89ad8e/a_high_detail_shot_of_a_cat_wearing_a_suit_realism_8k_-n_9_.png"
-        //image: "https://i.postimg.cc/gkM73gkt/laptop.png"
-        //        https%3A%2F%2Fi.postimg.cc%2FgkM73gkt%2Flaptop.png
-        image: query,
-        prompt: "Describe this contents of this image in the style of a product description. Be concise and limit to 2 sentences."
-      }),
-    });
-    msg.text = "Loading...";
-    window.speechSynthesis.speak(msg);
-    //console.log(response.json());
-    let prediction = await response.json();
+
+    try {
+       const response = await fetch("http://localhost:3000/api/predictions", {
+        mode: 'no-cors',
+        method: "POST",
+        headers: {
+          "Content-Type": "application/json",
+          Authorization: `Token ${process.env.REPLICATE_API_TOKEN}`,
+        },
+        body: JSON.stringify({
+          //image: "https://avatars.githubusercontent.com/u/147882946?v=4"
+          //image: "https://replicate.delivery/mgxm/8b4d747d-feca-477d-8069-ee4d5f89ad8e/a_high_detail_shot_of_a_cat_wearing_a_suit_realism_8k_-n_9_.png"
+          //image: "https://i.postimg.cc/gkM73gkt/laptop.png"
+          //        https%3A%2F%2Fi.postimg.cc%2FgkM73gkt%2Flaptop.png
+          image: "https://i.postimg.cc/gkM73gkt/laptop.png",
+          prompt: "Describe this contents of this image in the style of a product description. Be concise and limit to 2 sentences."
+        })
+      })
+      setData(await response.json());
+    } catch (error) {
+      console.log(error);
+      console.log(data);
+    }
+    let prediction = data;
     //console.log(prediction);
+    /*
     if (response.status !== 201) {
       setError(prediction.detail);
       return;
     }
+    */
     setPrediction(prediction);
 
+    /*
     while (
       prediction.status !== "succeeded" &&
       prediction.status !== "failed"
@@ -72,6 +72,7 @@ const ProductListing1Page = (props) => {
       console.log({prediction})
       setPrediction(prediction);
     }
+    */
     
   };
 
