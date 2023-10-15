7fd67efad7d6573820f98f95dc350937ec0b2a16
Grace Hu <gracehu2003@gmail.com>
2023-10-15 09:45:40 -0400

    resized product listing #5

--- verizonvox-react-app/verizonvox/src/components/ProductListing5.js
+++ verizonvox-react-app/verizonvox/src/components/ProductListing5.js
@@ -5,9 +5,9 @@ const ProductListing5 = () => {
   const imageUrl = process.env.PUBLIC_URL + '/verizon-product-images/samsung-galaxy-tab-a7lite.png';
   return (
     <div className="rounded-image-container">
-        <img src={imageUrl} alt="image 5" className= "shrink-image" />
+        <img src={imageUrl} alt="image 5" className= "shrink-image-5" />
         <div className="listing-info">
-            <h5 style={{ fontWeight: 'bold' }}>Samsung Galaxy Tab S7 FE 5G</h5>
+            <h5 style={{ fontWeight: 'bold' }}>Samsung Galaxy Tab S7 FE</h5>
             <h6 style={{ color: 'grey' }}>Retail price: $199.99</h6>
             <h6 style={{ color: 'grey' }}>3.9/5 stars</h6>
         </div>
